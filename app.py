b04a4a378c68d0a88255f07be05dcd5bd70e6a7f
Yoni <watch.black900@gmail.com>
2026-04-19 10:06:09 +0300

    Add transfer DBmango

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 from flask import Flask
-
+from pymongo import MongoClient
 from errors import errors_bp
 from routes import tasks_bp
-
-
-#===================Si tu veux travailler entièrement avec uuid changer tout ce qui est en rapport.
-
 
 # Main Flask app instance for the todo API.
 app = Flask(__name__)
@@ -13,6 +9,7 @@ app = Flask(__name__)
 # Register the task routes and shared error handlers.
 app.register_blueprint(tasks_bp)
 app.register_blueprint(errors_bp)
+
 
 
 if __name__ == "__main__":
